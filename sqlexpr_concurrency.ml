ccfb25d88d168a5872a4131ca29d2dbff5f41c16
Mauricio Fernandez <mfp@acm.org>
2011-03-08 13:16:17 +0100

    Sqlexpr_concurrency.THREAD: add mutex and associated ops to monad.

--- sqlexpr_concurrency.ml
+++ sqlexpr_concurrency.ml
@@ -10,6 +10,11 @@ sig
   val finalize : (unit -> 'a t) -> (unit -> unit t) -> 'a t
   val sleep : float -> unit t
   val auto_yield : float -> (unit -> unit t)
+
+  type mutex
+
+  val create_recursive_mutex : unit -> mutex
+  val with_lock : mutex -> (unit -> 'a t) -> 'a t
 end
 
 module Id =
@@ -29,4 +34,39 @@ struct
   let sleep dt = let _, _, _ = Unix.select [] [] [] dt in ()
 
   let auto_yield _ = (fun () -> ())
+
+  type mutex = unit
+  let create_recursive_mutex () = ()
+  let with_lock () f = f ()
+end
+
+
+module Lwt =
+struct
+  include Lwt
+  let auto_yield = Lwt_unix.auto_yield
+  let sleep = Lwt_unix.sleep
+
+  type mutex = { id : int; m : Lwt_mutex.t }
+
+  let new_id = let n = ref 0 in (fun () -> incr n; !n)
+
+  module LOCKS = Set.Make(struct
+                            type t = int
+                            let compare (x : int) y =
+                              if x < y then -1 else if x > y then 1 else 0
+                          end)
+  let locks = Lwt.new_key ()
+
+  let create_recursive_mutex () = { id = new_id (); m = Lwt_mutex.create () }
+
+  let with_lock m f =
+    match Lwt.get locks with
+        None ->
+          Lwt_mutex.with_lock m.m
+            (fun () -> Lwt.with_value locks (Some (LOCKS.singleton m.id)) f)
+      | Some s when LOCKS.mem m.id s -> f ()
+      | Some s ->
+          Lwt_mutex.with_lock m.m
+            (fun () -> Lwt.with_value locks (Some (LOCKS.add m.id s)) f)
 end
